bae0465b6ee6a1a3521cb7788eb57d52f8387833
aiswaryakv2595 <aiswaryakv2595@gmail.com>
2024-01-17 23:29:28 +0530

    design in progress

--- ecommerce-app/src/pages/Home.js
+++ ecommerce-app/src/pages/Home.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Marquee from "react-fast-marquee";
+import BlogCard from "../components/BlogCard";
+import ProductCard from "../components/ProductCard";
+import SpecialProducts from "../components/SpecialProducts";
 const Home = () => {
   return (
     <>
@@ -196,6 +199,46 @@ const Home = () => {
           </div>
         </div>
       </section>
+      <section className="featured-wrapper py-5 home-wrapper-2">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="section-heading">Featured Collection</h3>
+          </div>
+         <ProductCard/>
+         <ProductCard/>
+         <ProductCard/>
+         <ProductCard/>
+        </div>
+      </div>
+      </section>
+      <section className="special-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+        <div className="row">
+        <div className="col-12">
+            <h3 className="section-heading">Special Products</h3>
+          </div>
+        </div>
+        <div className="row">
+          <SpecialProducts/>
+          <SpecialProducts/>
+          <SpecialProducts/>
+        </div>
+        </div>
+      </section>
+      <section className="popular-wrapper py-5 home-wrapper-2">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="section-heading">Our Popular Products</h3>
+          </div>
+         <ProductCard/>
+         <ProductCard/>
+         <ProductCard/>
+         <ProductCard/>
+        </div>
+      </div>
+      </section>
       <section className="marque-wrapper py-5">
         <div className="container-xxl">
           <div className="row">
@@ -231,6 +274,19 @@ const Home = () => {
             </div>
           </div>
         </div>
+      </section>
+      <section className="blog-wrapper py-5 home-wrapper-2">
+      <div className="container-xxl">
+        <div className="row">
+          <div className="col-12">
+            <h3 className="section-heading">Our Latest Blogs</h3>
+          </div>
+          <BlogCard/>
+          <BlogCard/>
+          <BlogCard/>
+          <BlogCard/>
+        </div>
+      </div>
       </section>
     </>
   );
